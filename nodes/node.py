51e7e2ef6a1aebb925874915b7063ad848627d4b
sng70 <u56_jakmal_waw@technischools.com>
2024-05-02 21:23:33 +0200

    receiveMessage - method added

--- nodes/node.py
+++ nodes/node.py
@@ -2,21 +2,27 @@ import socket
 import threading
 
 class Node:
-    def __init__(self, ip, port, nickname, privateKey, publicKey, ):
+    def __init__(self, ip, port, nickname, privateKey, publicKey):
         self.ip = ip
         self.port = port
         self.nickname = nickname
         self.publicKey = publicKey
         self.privateKey = privateKey
 
-    def handleConenction(self):
+    def handleConenction(self, client_socket):
         pass
 
     def sendMessage(self, message):
         pass
 
-    def receiveMessage(self):
-        pass
+    def receiveMessage(self, client_socket):
+        expected_message_len = int(client_socket.recv(5).decode("utf-8"))
+        received_message = client_socket.recv(expected_message_len)
+
+        if len(received_message) != expected_message_len:
+            raise ConnectionError("Received message is not equal to expected message length")
+
+        return received_message.decode("utf-8")
 
     def forwardMessage(self, message):
         pass
