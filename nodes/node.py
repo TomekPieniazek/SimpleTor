4436df478d92e25b7a665a89a6313c145c181eee
sng70 <u56_jakmal_waw@technischools.com>
2024-05-02 21:04:35 +0200

    startNode method added

--- nodes/node.py
+++ nodes/node.py
@@ -1,3 +1,6 @@
+import socket
+import threading
+
 class Node:
     def __init__(self, ip, port, nickname, privateKey, publicKey, ):
         self.ip = ip
@@ -6,7 +9,7 @@ class Node:
         self.publicKey = publicKey
         self.privateKey = privateKey
 
-    def handeConenction(self):
+    def handleConenction(self):
         pass
 
     def sendMessage(self, message):
@@ -25,5 +28,13 @@ class Node:
         pass
 
     def startNode(self):
-        pass
+        node_socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+        node_socket.bind((self.ip, self.port))
+        node_socket.listen(10)
+
+        while True:
+            client_socket, address = node_socket.accept()
+
+            client_handler = threading.Thread(target=self.handleConenction, args=(client_socket,))
+            client_handler.start()
 
